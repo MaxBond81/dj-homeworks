4345c3fee5f2b6c6a600456f0c3921c61ecfd977
Bondarev Maxim <bonmax46@mail.ru>
2024-11-25 17:36:20 +0300

    create databases

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 
+from books.models import Book
+
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    books = Book.objects.all()
+    context = {"books_list": books}
+    return render(request, template, context)
+
+
+def books_pub_date_view(request, pub_date):
+    template = 'books/books_pagi.html'
+    books_pub_date = Book.objects.filter(pub_date=pub_date)
+    books_next = (Book.objects.filter(pub_date__gt=pub_date).order_by('pub_date').first())
+    if books_next:
+        books_next = str(books_next.pub_date)
+    else:
+        books_next = "Дальше нет дат"
+    books_previous = Book.objects.filter(pub_date__lt=pub_date).order_by('pub_date').first()
+    if books_previous:
+        books_previous = str(books_previous.pub_date)
+    else:
+        books_previous = "Дальше нет дат"
+    context = {"books_list": books_pub_date,
+               "books_next": books_next,
+               "books_previous": books_previous,
+               }
     return render(request, template, context)
--- 2.1-databases/work_with_database/main/settings.py
+++ 2.1-databases/work_with_database/main/settings.py
@@ -51,11 +51,9 @@ MIDDLEWARE = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_import_phones',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': 'demoorm',
         'USER': 'postgres',
-        'PASSWORD': 'ваш пароль',
+        'PASSWORD': '',
     }
 }
 
